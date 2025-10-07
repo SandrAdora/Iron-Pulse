1d9147ad96d93ec8d27237188169dfe188dab923
GoodMorningMidnightMirados <83751069+SandrAdora@users.noreply.github.com>
2025-10-07 10:54:09 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 export default function Layout(props) 
 
 {
-    const {children} =  props
+    const { children } =  props
     const header = (
         <header>
-            <h1 className="text-gradient">The Iron Pulse</h1>
+            <h1 className="text-gradient">The Iron Pulse with Lorand</h1>
             <p><strong>The 30 Simple Workouts Program</strong></p>
 
         </header>
